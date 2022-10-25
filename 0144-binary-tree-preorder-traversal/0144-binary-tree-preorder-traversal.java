561b55b3ab70be3c8c42b975040856accad09338
Tanmoy Mahato <tanmoy.mato@gmail.com>
2022-10-25 17:07:00 +0530

    Update 0144-binary-tree-preorder-traversal.java

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+
+/* Using RECURSION */
 class Solution {
     
     public void traverse (TreeNode root, ArrayList<Integer> preorderList){
@@ -29,6 +31,35 @@ class Solution {
         ArrayList<Integer> preorderList = new ArrayList<>();
         
         traverse(root, preorderList);
+        return preorderList;
+    }
+}
+
+
+
+
+/* Using ITERATION */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> preorderList = new ArrayList<>();
+        
+        if(root == null) //if tree is empty return empty list
+            return preorderList;
+        
+        Stack<TreeNode> stk = new Stack<>();
+        stk.push(root); //push the root node to stack
+        
+        while(!stk.isEmpty()){
+            TreeNode node = stk.pop();  //pop every element from sstack
+            preorderList.add(node.val); //and push it into list
+            
+            if(node.right != null) //if popped node has right node, push in stack
+                stk.push(node.right);
+            if(node.left != null) //if popped node has left node, push in stack
+                stk.push(node.left);
+             
+        }
+        
         return preorderList;
     }
 }
