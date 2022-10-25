c7c2c6035d6e3fb015a4293777b37fd79c754797
Tanmoy Mahato <tanmoy.mato@gmail.com>
2022-10-25 17:58:12 +0530

    Update 0094-binary-tree-inorder-traversal.java

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+
+/* Using RECURSION */
 class Solution {
     
     public void traverse(TreeNode root, ArrayList<Integer> inorderList){
@@ -26,6 +28,42 @@ class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> inorderList = new ArrayList<>();
         traverse(root, inorderList);
+        return inorderList;
+    }
+}
+
+
+
+
+/* Using ITERATION */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> inorderList = new ArrayList<>();
+        
+        if(root == null)
+            return inorderList;
+        
+        Stack<TreeNode> stk = new Stack<>();
+        TreeNode node = root; //store the root node 
+        
+        while(true){
+            if(node != null){ //if node is !null push node in stack and move left
+                stk.push(node);
+                node = node.left;
+            }
+            else{ //if node is null 
+                //check if stack is empty, if empty, tree traversal is finished
+                if(stk.isEmpty())
+                    break;
+                //else if stack is not empty, pop the top node, add its value in list, and move right
+                else{ 
+                    node = stk.pop();
+                    inorderList.add(node.val);
+                    node = node.right;
+                }                
+            }
+        }
+        
         return inorderList;
     }
 }
